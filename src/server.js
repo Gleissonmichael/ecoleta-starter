001727eee66932c0ce2c1319ebace48178cd19ac
Gleisson Michael <gleisson.michael@hotmail.com>
2020-06-15 15:58:01 -0300

    create routes and add nunjucks

--- src/server.js
+++ src/server.js
@@ -3,8 +3,19 @@ const server = express();
 
 server.use(express.static("public"))
 
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
 })
 
 server.listen(3000);
